869b26742d25040ca7ef1f424da35c8f1fccd9ff
R-Von <778615648@qq.com>
2020-07-20 10:49:28 +0800

    feat():更新 Clomun组件 代码

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,138 @@ import 'package:flutter/material.dart';
 //   runApp(ListApps());
 // }
 
-void main () => runApp(GridImg());
+void main () => runApp(BasicColumn());
+
+class BasicColumn extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title:'Basic Column',
+      home: Scaffold(
+        appBar:new AppBar(
+          title:new Text('Column Layout')
+        ),
+        body:new Column(
+          children:<Widget>[
+            new Text('Red',style: TextStyle(color:Colors.redAccent),),
+            new Text('Orange',style: TextStyle(color:Colors.orangeAccent),),
+            new Text('Yellow',style: TextStyle(color:Colors.yellowAccent),),
+            new Text('Green',style: TextStyle(color:Colors.greenAccent),),
+            new Text('Blue',style: TextStyle(color:Colors.blueAccent),),
+          ]
+        )
+      ),
+    );
+  }
+}
+
+
+
+
+
+class MixBtn extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title:'1',
+      home:Scaffold(
+        appBar:new AppBar(title:new Text('Mix Layout')),
+        body:new Row(
+          children:<Widget> [
+            new RaisedButton(
+               onPressed:(){},
+              color:Colors.redAccent,
+              child:new Text('Button-One',style:TextStyle(color:Colors.white))
+            ),
+            Expanded(child: new RaisedButton(
+              onPressed:(){},
+              color:Colors.greenAccent,
+              child: new Text('Button-Two',style:TextStyle(color:Colors.white)),
+            )),
+            new RaisedButton(
+               onPressed:(){},
+              color:Colors.redAccent,
+              child:new Text('Button-Three',style:TextStyle(color:Colors.white))
+            )
+          ]
+        )
+      )
+    );
+  }
+}
+
+
+
+
+class BtnLayouts extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title:'BtnLayouts',
+      home: Scaffold(
+        appBar:new AppBar(
+          title:new Text('Btns Layouts')
+        ),
+        body:new Row(
+          children:<Widget>[
+            Expanded(child: new RaisedButton(
+              onPressed:(){},
+              color:Colors.redAccent,
+              child:new Text('Red Btn')
+            )),
+            Expanded(child:new RaisedButton(
+              onPressed: (){},
+              color:Colors.orangeAccent,
+              child:new Text('Orange Btn')
+            )),
+            Expanded(child: new RaisedButton(
+              onPressed:(){},
+              color:Colors.yellowAccent,
+              child: new Text('Yellow Btn'),
+            ))
+          ]
+        )
+      ),
+    );
+  }
+}
+
+
+
+
+class BtnLayout extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title:'Btn Layout',
+      home: Scaffold(
+        appBar:new AppBar(
+          title:new Text('水平方向布局')
+        ),
+        body:new Row(
+          children:<Widget>[
+            new RaisedButton(
+              onPressed:(){},
+              color:Colors.redAccent,
+              child:new Text('Red-Btn')
+            ),
+             new RaisedButton(
+              onPressed:(){},
+              color:Colors.orangeAccent,
+              child:new Text('Orange-Btn')
+            ),
+             new RaisedButton(
+              onPressed:(){},
+              color:Colors.yellowAccent,
+              child:new Text('Yellow-Btn')
+            ),
+          ]
+        )
+      ),
+    );
+  }
+}
+
 
 
 
@@ -68,10 +199,6 @@ class GridViews extends StatelessWidget {
  
 
 
-
-// void main ()=> runApp(ListsViews(
-//   items:new List<String>.generate(1000, (i) => 'Item $i')
-// ));
 
 class ListsViews extends StatelessWidget {
   final List<String> items;
